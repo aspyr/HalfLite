8bc06ed1b52dfbf3c05db9853041a68e58b65995
James Barnhill <jamesbrnhll@gmail.com>
2013-09-17 16:54:44 -0500

    Initial push

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="style2.css" rel="stylesheet">
+        <title>HalfLite</title>
+        <link rel="icon" type="image/png" href="favicon.png">
+</head>
+<body>
+    <div id="header">
+        <div id="logo">
+            <img id="hl" src="hl3.png">
+        </div>
+        <div id="menu">
+            <ul>
+                <a href="http://www.youtube.com/user/trioProductions90" target="_blank"><li>YOUTUBE.</li></a>
+                <a href="http://www.facebook.com/halfliteinc" target="_blank"><li>FACEBOOK.</li></a>
+                <a href="http://twitter.com/half_films" target="_blank"><li>TWITTER.</li></a>
+		<a href="http://goo.gl/U24ej" target="_blank"><li>GOOGLE+.</li></a>
+                <a href="http://goo.gl/fAUJN" target="_blank"><li>PICASA.</li></a>
+            </ul>
+            <?php //include('menu.php'); ?>
+        </div>
+    </div>
+    <div id="content"><div id='twitter_update_list'>
+</div><script type="text/javascript" src="http://twitter.com/javascripts/blogger.js">
+</script>
+<script type="text/javascript" src="https://api.twitter.com/1/statuses/user_timeline/half_films.json?callback=twitterCallback2&count=1">
+</script></div>
+    <div id="footer">© 2012 HALFLITE</div>
+</body>
+</html>
